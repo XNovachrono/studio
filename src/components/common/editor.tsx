600434b67588ece2610df6428940b4b733d41e1e
Xd Xd <xdxd63165@gmail.com>
2025-10-17 21:22:00 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@t

--- src/components/common/editor.tsx
+++ src/components/common/editor.tsx
@@ -6,7 +6,7 @@ import { useEditor, EditorContent, Editor as TiptapEditor, BubbleMenu } from "@t
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import Image from "@tiptap/extension-image";
-import Link from "@tiptap_extension-link";
+import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
 import TextStyle from '@tiptap/extension-text-style';
 import { Color } from '@tiptap/extension-color';
