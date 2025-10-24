c4548c6fdb78cd3f6ed1542506e8622cf98e5402
Xd Xd <xdxd63165@gmail.com>
2025-10-24 18:43:51 +0000

    Try fixing this error: `Console Error: Error: [GoogleGenerativeAI Error]

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,7 +5,7 @@ import {enableFirebaseTelemetry} from '@genkit-ai/firebase';
 
 enableFirebaseTelemetry();
 
-export const gemini15Flash = googleAI.model('gemini-1.5-flash');
+export const gemini15Flash = googleAI.model('gemini-1.5-flash-latest');
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GOOGLE_GENAI_API_KEY})],
